d2f321d99d08ca4cd4d5f4c2cf315d50749ff5f3
Elliott Mestas <elliott.mestas@gmail.com>
2015-06-30 00:44:09 -0400

    added tests for fibonacci and xpowery

--- .guides/tests/guide-script.js
+++ .guides/tests/guide-script.js
@@ -1,5 +1,5 @@
 function t_print0ToN(data) {
-  TESTS.SimpleOutputTest(data, [{min: 0, max: 20}], function(inp, vars) {
+  TESTS.SimpleOutputTestWithInputRange(data, {min: 0, max: 20}, function(inp, vars) {
     var expected = [0];
     for (var i = 1; i <= inp[0]; ++i) {
       expected.push(i);
@@ -9,7 +9,7 @@ function t_print0ToN(data) {
 }
 
 function t_printNTo0(data) {
-  TESTS.SimpleOutputTest(data, [{min: 0, max: 20}], function(inp, vars) {
+  TESTS.SimpleOutputTestWithInputRange(data, {min: 0, max: 20}, function(inp, vars) {
     var expected = [0];
     for (var i = 1; i <= inp[0]; ++i) {
       expected.unshift(i);
@@ -19,7 +19,7 @@ function t_printNTo0(data) {
 }
 
 function t_timesTable(data) {
-  TESTS.SimpleOutputTest(data, [{min: 0, max: 16}], function(inp, vars) {
+  TESTS.SimpleOutputTestWithInputRange(data, {min: 0, max: 16}, function(inp, vars) {
     var expected = [];
     if (inp[0] <= 12) {
       for (var i = 1; i <= 12; ++i) {
@@ -33,7 +33,7 @@ function t_timesTable(data) {
 }
 
 function t_x2(data) {
-  if (TESTS.CountNumInstructionsOfType(data, CRUNCH.INSTRUCTION_TYPE_OUT) > 2) {
+  if (TESTS.CountNumInstructionsOfType(data, CRUNCH.INSTRUCTION_TYPE_OUT) > 1) {
     TESTS.ShowFail('You are not allowed to use more than one OUT for this test.');
     return;
   }
@@ -43,18 +43,44 @@ function t_x2(data) {
 }
 
 function t_10div2(data) {
-  if (TESTS.CountNumInstructionsOfType(data, CRUNCH.INSTRUCTION_TYPE_DIV) > 1) {
+  if (TESTS.CountNumInstructionsOfType(data, CRUNCH.INSTRUCTION_TYPE_DIV) > 0) {
     TESTS.ShowFail('You are not allowed to use any DIV instructructions for this test.');
     return;
   }
-  var inp1 = TESTS.GetRandomIntegerArray(1, 1000)[0];
-  var inp2 = TESTS.GetRandomIntegerArray(1, 50)[0];
-  var variables = [{name: 'X', value: inp1}, {name: 'Y', value: inp2}];
   TESTS.SimpleOutputTest(data, [{name: 'X', min: 0, max: 1000}, {name: 'Y', min: 1, max: 50}],
     function(inp, vars) {
       return [Math.floor(vars[0].value / vars[1].value)]
     }
   );
+}
+
+function t_xPowerY(data) {
+  TESTS.SimpleOutputTestWithInputRange(data, {num: 2, min: -1, max: 10}, function(inp, vars) {
+    if (inp[0] < 0 || inp[1] < 0) {
+      return [-1];
+    }
+    return [Math.pow(inp[0], inp[1])];
+  });
+}
+
+function t_fibonacci(data) {
+  var fibonacci = function(iterations, output, val1, val2) {
+    if (iterations === 0) {
+      return val1;
+    }
+    output.push(val1);
+    output.push(fibonacci(iterations - 1, output, val2, val1 + val2));
+  }
+  
+  TESTS.SimpleOutputTestWithInputRange(data, {min: 0, max: 20}, function(inp, vars) {
+    var expected = [];
+    fibonacci(inp[0], expected, 0, 1);
+    return expected;
+  });
+}
+
+function t_nFactorial(data) {
+
 }
 
 TESTS.SetupButtonTest();
