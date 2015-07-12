7050fb505674413479a22e7939698ef4844894f3
Elliott Mestas <elliott.mestas@gmail.com>
2015-07-12 22:25:49 +0200

    required changes to work with new browserify solution

--- .guides/tests/guide-script.js
+++ .guides/tests/guide-script.js
@@ -1,5 +1,5 @@
 function t_print0ToN(data) {
-  TESTS.SimpleOutputTestWithInputRange(data, {min: 0, max: 20}, function(inp, vars) {
+  tests.SimpleOutputTestWithInputRange(data, {min: 0, max: 20}, function(inp, vars) {
     var expected = [0];
     for (var i = 1; i <= inp[0]; ++i) {
       expected.push(i);
@@ -9,7 +9,7 @@ function t_print0ToN(data) {
 }
 
 function t_printNTo0(data) {
-  TESTS.SimpleOutputTestWithInputRange(data, {min: 0, max: 20}, function(inp, vars) {
+  tests.SimpleOutputTestWithInputRange(data, {min: 0, max: 20}, function(inp, vars) {
     var expected = [0];
     for (var i = 1; i <= inp[0]; ++i) {
       expected.unshift(i);
@@ -19,7 +19,7 @@ function t_printNTo0(data) {
 }
 
 function t_timesTable(data) {
-  TESTS.SimpleOutputTestWithInputRange(data, {min: 1, max: 16}, function(inp, vars) {
+  tests.SimpleOutputTestWithInputRange(data, {min: 1, max: 16}, function(inp, vars) {
     var expected = [];
     if (inp[0] <= 12) {
       for (var i = 1; i <= 12; ++i) {
@@ -33,21 +33,21 @@ function t_timesTable(data) {
 }
 
 function t_x2(data) {
-  if (TESTS.CountNumInstructionsOfType(data, CRUNCH.INSTRUCTION_TYPE_OUT) > 1) {
-    TESTS.ShowFail('You are not allowed to use more than one OUT for this test.');
+  if (tests.CountNumInstructionsOfType(data, CRUNCH.INSTRUCTION_TYPE_OUT) > 1) {
+    tests.ShowFail('You are not allowed to use more than one OUT for this test.');
     return;
   }
-  TESTS.SimpleOutputTest(data, [], function(inp, vars) {
+  tests.SimpleOutputTest(data, [], function(inp, vars) {
     return [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
   });
 }
 
 function t_10Div2(data) {
-  if (TESTS.CountNumInstructionsOfType(data, CRUNCH.INSTRUCTION_TYPE_DIV) > 0) {
-    TESTS.ShowFail('You are not allowed to use any DIV instructructions for this test.');
+  if (tests.CountNumInstructionsOfType(data, CRUNCH.INSTRUCTION_TYPE_DIV) > 0) {
+    tests.ShowFail('You are not allowed to use any DIV instructructions for this test.');
     return;
   }
-  TESTS.SimpleOutputTest(data, [{name: 'X', min: 0, max: 1000}, {name: 'Y', min: 1, max: 50}],
+  tests.SimpleOutputTest(data, [{name: 'X', min: 0, max: 1000}, {name: 'Y', min: 1, max: 50}],
     function(inp, vars) {
       return [Math.floor(vars[0].value / vars[1].value)]
     }
@@ -55,7 +55,7 @@ function t_10Div2(data) {
 }
 
 function t_xPowerY(data) {
-  TESTS.SimpleOutputTestWithInputRange(data, {num: 2, min: -1, max: 10}, function(inp, vars) {
+  tests.SimpleOutputTestWithInputRange(data, {num: 2, min: -1, max: 10}, function(inp, vars) {
     if (inp[0] < 0 || inp[1] < 0) {
       return [-1];
     }
@@ -71,7 +71,7 @@ function t_fibonacci(data) {
     return val1;
   }
   
-  TESTS.SimpleOutputTestWithInputRange(data, {min: 0, max: 20}, function(inp, vars) {
+  tests.SimpleOutputTestWithInputRange(data, {min: 0, max: 20}, function(inp, vars) {
     var expected = [];
     fibonacci(inp[0], expected, 0, 1);
     expected.push(0);
@@ -81,7 +81,7 @@ function t_fibonacci(data) {
 }
 
 function t_nFactorial(data) {
-  TESTS.SimpleOutputTestWithInputRange(data, {min: 0, max: 12}, function(inp, vars) {
+  tests.SimpleOutputTestWithInputRange(data, {min: 0, max: 12}, function(inp, vars) {
     var expected = 1;
     for (var i = 1; i <= inp[0]; ++i) {
       expected *= i;
@@ -91,11 +91,11 @@ function t_nFactorial(data) {
 }
 
 function waitForCrunchScript() {
-  if (!window.CRUNCH_COMMON_LOADED) {
+  if (!window.TESTS_COMMON_LOADED) {
     setTimeout(waitForCrunchScript, 100);
     return;
   }
-  TESTS.SetupButtonTest();
+  tests.SetupButtonTest();
 }
 
 waitForCrunchScript();
